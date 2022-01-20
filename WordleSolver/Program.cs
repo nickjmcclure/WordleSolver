c0259412c1380a3315be4f1e1970fa25ae45401b
Nick McClure <nickjm@uky.edu>
2022-01-19 23:23:56 -0500

    Add project files.

---
+++ WordleSolver/Program.cs
@@ -0,0 +1,51 @@
+﻿using WorldSolver.Common;
+
+
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var solver = new Solver(5);
+solver.AddGuess("arose");
+solver.AddGuess("noily");
+solver.AddGuess("coign");
+
+
+//solver.AddKnown(0, 'p');
+solver.AddKnown(1, 'o');
+solver.AddKnown(2, 'i');
+//solver.AddKnown(3, 's');
+//solver.AddKnown(4, 'e');
+
+
+
+solver.AddContains('n');
+//solver.AddContains('o');
+//solver.AddContains('d');
+//solver.AddContains('y');
+//solver.AddContains('l');
+
+solver.AddInvalid('a');
+solver.AddInvalid('r');
+solver.AddInvalid('s');
+solver.AddInvalid('e');
+solver.AddInvalid('l');
+solver.AddInvalid('y');
+///solver.AddInvalid('l');
+//solver.AddInvalid('y');
+solver.AddInvalid('c');
+solver.AddInvalid('g');
+
+
+var output = solver.GetOptions();
+Console.WriteLine(output.Count);
+
+Console.WriteLine(solver.GetGuess());
+
+/*
+Console.WriteLine("all words---");
+foreach (var item in output)
+{
+    Console.WriteLine(item);
+}
+*/
---
+++ WorldSolver.Common/Solver.cs
@@ -0,0 +1,155 @@
+﻿using System.Linq;
+
+namespace WorldSolver.Common
+{
+    public class Solver
+    {
+        /// <summary>
+        /// List containing the guesses
+        /// </summary>
+        public SortedList<int,string> Guesses { get; private set; }
+
+        /// <summary>
+        /// List of known characters by their position
+        /// </summary>
+        public SortedList<int, char> Known { get; private set; }
+
+        /// <summary>
+        /// List of charactors that are in the word, but not the correct position
+        /// </summary>
+        public List<char> Contains { get; private set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public List<char> Invalid { get; private set; }
+
+        public Dictionary<char, int> LetterCount
+        {
+            get
+            {
+                string alphabet = "abcdefghijklmnopqrstuvwxyz";
+                Dictionary<char, int> output = new();
+                var options = GetOptions();
+                foreach (var letter in alphabet)
+                {
+                    int count = 0;
+                    foreach (var value in options)
+                    {
+                        count += value.Count(s => s == letter);
+                    }
+                    output.Add(letter, count);
+                }
+                return output;
+            }
+        }
+
+        private readonly List<string> _dictionary;
+        private int _guess;
+
+        public Solver(int Length)
+        {
+            Guesses = new SortedList<int, string>();
+            Known = new SortedList<int, char>();
+            Contains = new List<char>();
+            Invalid = new List<char>();
+
+            _guess = 0;
+            
+            _dictionary = new List<string>();
+            ReadFile(Length);
+        }
+
+        public void AddGuess(string Input)
+        {
+            _guess++;
+            Guesses.Add(_guess, Input);
+        }
+
+        public void AddKnown(int Position, char Value)
+        {
+            Known.Add(Position, Value);
+        }
+
+        public void AddContains(char Value)
+        {
+            Contains.Add(Value);
+        }
+
+        public void AddInvalid(char Value)
+        {
+            Invalid.Add(Value);
+        }
+
+        public List<string> GetOptions()
+        {
+
+            var output = _dictionary.AsQueryable();
+            
+            foreach (var kvp in Known)
+            {
+                output = output.Where(s => s[kvp.Key] == kvp.Value);
+            }
+
+            foreach (var item in Invalid)
+            {
+                output = output.Where(s => !s.Contains(item));
+            }
+
+            foreach (var item in Contains)
+            {
+                output = output.Where(s => s.Contains(item));
+
+                foreach (var guess in Guesses.Values)
+                {
+                    int pos = guess.IndexOf(item);
+                    if (pos > -1)
+                    {
+                        output = output.Where(s => s[pos] != item);
+                    }
+                }
+            }
+
+            output = output.Except(Guesses.Values);
+            return output.ToList();
+        }
+
+        public string GetGuess()
+        {
+            var options = GetOptions();
+            var letterCount = LetterCount;
+            Dictionary<string, int> wordWeight = new();
+            
+            foreach (var option in options)
+            {
+                int weight = 0;
+                foreach (var letter in option.Distinct())
+                {
+                    weight += letterCount[letter];
+                }
+                wordWeight.Add(option, weight);
+            }
+
+            return wordWeight.OrderByDescending(w => w.Value).First().Key;
+        }
+
+        private void ReadFile(int Length, string Filename = "words_alpha.txt")
+        {
+            List<string> invalid = new() { "nilot" };
+            using StreamReader reader = new(Filename);
+            int x = 1;
+            while (!reader.EndOfStream)
+            {
+                string? word = reader.ReadLine();
+                if (!string.IsNullOrWhiteSpace(word) && word.Length == Length)
+                {
+                    //Console.WriteLine($"{x} - {word}");
+                    _dictionary.Add(word);
+                    x++;
+                }
+            }
+            _dictionary.RemoveAll(w => invalid.Contains(w));
+            Console.WriteLine($"Loaded {_dictionary.Count} words");
+        }
+    }
+}
